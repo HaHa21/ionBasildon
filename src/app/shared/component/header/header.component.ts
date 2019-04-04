4190405aeb6c5a1ec8e152a67acd595ff883d7a8
Dominic Lee <domlee2012@gmail.com>
2019-04-04 23:51:23 +0800

    renamed home page to login page & created header component

---
+++ src/app/shared/component/header/header.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Input } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss'],
+})
+export class HeaderComponent implements OnInit {
+  @Input() options? : HeaderOptions;
+  
+  constructor() { }
+
+  ngOnInit() {}
+
+}
+
+export interface HeaderOptions{
+
+}
